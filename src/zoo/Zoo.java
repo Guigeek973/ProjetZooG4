ad912545e579226cf52df8ed7e61976f06923728
Unknown <guillaume.ostorero@gmail.com>
2018-05-07 16:54:11 +0200

    zoo

--- src/zoo/Zoo.java
+++ src/zoo/Zoo.java
@@ -25,7 +25,19 @@ public class Zoo {
 		return null;
 	}
 	
-	//modifier aléatoirement l’état de certains animaux (les rendre malades, les endormir, etc.) ;
-	//modifier aléatoirement l’état de certains enclos (leur propreté, leur salinité, etc.) ;
-	//passer la main à l'employé (et donc à l’utilisateur) pour qu'il s'occupe du zoo (son nombre d’action par intervalle de temps devant être limité).
+	
+	public void modifierEtatAnimaux() {
+		
+		// (les rendre malades, les endormir, etc.) aleatoire
+	}
+	public void modifierEtatEnclos() {
+		
+		// (leur propreté, leur salinité, etc.) aleatoire
+	}
+	
+	public void donnerLaMain() {
+		//passer la main à l'employé (et donc à l’utilisateur) pour qu'il s'occupe du zoo (son nombre d’action par intervalle de temps devant être limité).
+	}
+	
 }
+	
